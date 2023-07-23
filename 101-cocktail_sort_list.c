65ab272362a06b312a8a22c78a196a88176f367d
kokainaW <williamkhoko@yahoo.com>
2023-07-23 23:31:03 +0000

    cocktail sorting list

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swap - Will Swap A Node With The Next Node In The List
+ * @list: A Double Pointer To The Beginning Of The List
+ * @node: A Node To Swap
+ *
+ * Return: void
+ */
+void swap(listint_t **list, listint_t *node)
+{
+	node->next->prev = node->prev;
+	if (node->prev)
+		node->prev->next = node->next;
+	else
+		*list = node->next;
+	node->prev = node->next;
+	node->next = node->next->next;
+	node->prev->next = node;
+	if (node->next)
+		node->next->prev = node;
+}
+
+/**
+ * cocktail_sort_list - Will Sort A Doubly Linked List Of Integers In Ascending
+ * Order Using The Cocktail Shaker Sort Algorithm
+ * @list: Double pointer To The Head Of The Doubly Linked List
+ *
+ * Return: void
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	char swapped = 1;
+	listint_t *temp;
+
+	if (list == NULL || *list == NULL)
+		return;
+	temp = *list;
+	while (swapped != 0)
+	{
+		swapped = 0;
+		while (temp->next != NULL)
+		{
+			if (temp->next->n < temp->n)
+			{
+				swap(list, temp);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				temp = temp->next;
+		}
+		if (swapped == 0)
+			break;
+		swapped = 0;
+		while (temp->prev != NULL)
+		{
+			if (temp->prev->n > temp->n)
+			{
+				swap(list, temp->prev);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				temp = temp->prev;
+		}
+	}
+}
